60417f5cc78ca1060f81036b8e5b45a562cbfb6e
Artem GRIGORYEV <artgrigs1993@gmail.com>
2024-06-10 17:23:14 +0200

    Updated fast.py to include endpoint for image predict and updated preprocessing

--- FER_directory/API/fast.py
+++ FER_directory/API/fast.py
@@ -1,54 +1,46 @@
-#import requests
-
 from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import Response
-
 import numpy as np
-#import cv2
 import io
-#from face_rec.face_detection import annotate_face
+import cv2
+from tensorflow.keras import models
+from starlette.responses import Response
+from FER_directory.logic_ml.preprocessing import process_image, decode_predictions, image_with_bounding_boxes
+
+import os
+
+
+
 
 app = FastAPI()
 
-# Define a root `/` endpoint
-@app.get('/')
-def index():
-    return {'ok': True}
-
-@app.get('/predict')
-def predict():
-    return {'wait': 64}
-
-
-# url = 'http://localhost:8000/predict'
-
-# params = {
-#     'day_of_week': 0, # 0 for Sunday, 1 for Monday, ...
-#     'time': '14:00'
-# }
-
-# response = requests.get(url, params=params)
-# res = response.json() #=> {'wait': 64}
-# prediction = res['wait']
-
-# @app.post('/upload_image')
-# async def receive_image(img: UploadFile=File(...)):
-#     ### Receiving and decoding the image
-#     contents = await img.read()
-
-#     nparr = np.fromstring(contents, np.uint8)
-#     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
-
-#     ### Do cool stuff with your image.... For example face detection
-#     #annotated_img = annotate_face(cv2_img)
-
-#     ### Encoding and responding with the image
-#     im = cv2.imencode('.png')[1] # extension depends on which format is sent from Streamlit
-#     return Response(content=im.tobytes(), media_type="image/png")
-
 @app.post('/upload_image')
-def receive_image(img: UploadFile=File(...)):
+async def receive_image(img: UploadFile = File(...)):
     ### Receiving and decoding the image
-    contents = img.read()
-    return contents
+    image = await img.read()
+
+    nparr = np.fromstring(image, np.uint8)
+    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
+
+
+    X_image, boxes = process_image(cv2_img)
+
+    # GET FER PREDICTIONS
+    path_model = os.path.dirname(os.path.dirname(os.getcwd()))
+    path_model = os.path.join(path_model,'models_trained/ResNet50V2_my_model_VM.h5')
+
+    fer_model = models.load_model(path_model)
+    predictions = fer_model.predict(X_image)
+
+    #UPDATE ORIGINAL PHOTO WITH BOUNDING BOXES AND EMOTION LABEL
+
+    labels = decode_predictions(predictions)
+
+
+
+    buf = io.BytesIO()
+    image_finale = image_with_bounding_boxes(cv2_img, boxes, labels)
+    image_finale = np.array(image_finale)
+
+    im = cv2.imencode('.jpg',image_finale)[1]
+
+    return Response(content=im.tobytes(), media_type="image/png")
