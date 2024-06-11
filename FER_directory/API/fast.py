fcf35450f5b438a19b61d5e83c1567a718f39f0a
Jean Schindler <jeanlucaschindler@gmail.com>
2024-06-11 10:18:14 +0200

    added endpoint for video

--- FER_directory/API/fast.py
+++ FER_directory/API/fast.py
@@ -4,12 +4,11 @@ import io
 import cv2
 from tensorflow.keras import models
 from starlette.responses import Response
-from FER_directory.logic_ml.preprocessing import process_image, decode_predictions, image_with_bounding_boxes
-
+from starlette.responses import FileResponse
+from FER_directory.logic_ml.preprocessing_photo import process_image, decode_predictions, image_with_bounding_boxes
+from FER_directory.logic_ml.preprocessing_video import annotate_frame_with_boxes
+import tempfile
 import os
-
-
-
 
 app = FastAPI()
 
@@ -44,3 +43,63 @@ async def receive_image(img: UploadFile = File(...)):
     im = cv2.imencode('.jpg',image_finale)[1]
 
     return Response(content=im.tobytes(), media_type="image/png")
+
+
+@app.post('/upload_video')
+async def receive_video(vid: UploadFile = File(...)):
+    # Save the uploaded video to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp:
+        tmp.write(await vid.read())
+        video_path = tmp.name
+
+    # Create a temporary file for the output video
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".avi") as temp_output:
+        out_path = temp_output.name
+
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(out_path, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+
+    # Load the pre-trained model
+    path_model = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'models_trained', 'ResNet50V2_my_model_VM.h5')
+    fer_model = models.load_model(path_model)
+
+    frame_count = 0
+    last_predictions = None
+    last_boxes = None
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Only process every 'frame_skip' frames
+        if frame_count % 16 == 0:
+            faces, boxes = process_image(frame)
+            if faces.size != 0:
+                predictions = fer_model.predict(faces)
+                labels = decode_predictions(predictions)
+                last_predictions = labels
+                last_boxes = boxes
+            else:
+                last_predictions = None
+                last_boxes = None
+
+        # Annotate the frame
+        if last_predictions is not None and last_boxes is not None:
+            annotated_frame = annotate_frame_with_boxes(frame, last_boxes, last_predictions)
+        else:
+            annotated_frame = frame
+
+        out.write(annotated_frame)
+        frame_count += 1
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+    # Clean up the input video file
+    os.remove(video_path)
+
+    return FileResponse(out_path, media_type='video/avi', filename='processed_video.avi')
